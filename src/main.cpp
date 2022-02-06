5a863d4c73eb7736325c7d9f1692ed3fd3c4644a
Fernando Fonseca <40750101+TheUnnamedPerson@users.noreply.github.com>
2022-02-06 09:32:06 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -56,42 +56,9 @@ void pre_auton(void) {
 
 void autonomous(void) {
   // code for picking up mogo
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//                                                                                     //
-//                               Driver-Based Control                                  //
-//                                                                                     //
-/////////////////////////////////////////////////////////////////////////////////////////
-
-void drivercontrol(void) {
-  Brain.Screen.clearScreen();
-  Brain.Screen.print("Hello World!\n");
-
-  int mogoIntakePercent = 25;
-
-  while (true) {
-    // spin to win
-    // Left Drive Control (Left Joystick)
-    frontLeftDrive.spin(vex::directionType::fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
-    backLeftDrive.spin(vex::directionType::fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
-
-    // Left Drive Control (Right Joystick)
-    frontRightDrive.spin(vex::directionType::fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
-    backRightDrive.spin(vex::directionType::fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
-
-    if (Controller1.ButtonR1.pressing())
-    {
-      // Ring Intake Control (ButtonUp and ButtonDown)
-      frontLeftMogoIntake.spin(vex::directionType::fwd, mogoIntakePercent, vex::velocityUnits::pct);
-      frontRightMogoIntake.spin(vex::directionType::fwd, mogoIntakePercent, vex::velocityUnits::pct);
-    }
-    else if (Controller1.ButtonR2.pressing()) {
-      // Ring Intake Control (ButtonUp and ButtonDown)
-      frontLeftMogoIntake.spin(vex::directionType::rev, mogoIntakePercent, vex::velocityUnits::pct);
-      frontRightMogoIntake.spin(vex::directionType::rev, mogoIntakePercent, vex::velocityUnits::pct);
-    }
-    else {
+  float r = 3.5;
+	float rpm = 200;
+`    else {
       frontLeftMogoIntake.stop(vex::brakeType::brake);
       frontRightMogoIntake.stop(vex::brakeType::brake);
     }
